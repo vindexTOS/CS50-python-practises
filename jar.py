9cb9271d047dc62bdc873538cb3ca32c9c89ff2c
VindexTOS <giorgikutateladze1998@gmail.com>
2023-09-26 23:24:40 +0400

    mario

---
+++ jar.py
@@ -0,0 +1,5 @@
+arr =  'Hello World' 
+
+
+print(  arr.split()[0][0:1]   )
+
---
+++ mario.py
@@ -0,0 +1,16 @@
+val = int(input("Height: "))
+
+
+i = 0
+
+while i < val:
+     
+    i+=1
+    for x in range(4):
+        print("#",end="")
+    
+   
+        
+     
+        
+# python mario.py
