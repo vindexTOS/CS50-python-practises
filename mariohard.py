086e917695de5ce3786c65bef51bceb24d08c4af
VindexTOS <giorgikutateladze1998@gmail.com>
2023-09-27 18:44:15 +0400

    mario two

---
+++ mariohard.py
@@ -0,0 +1,27 @@
+val = int(input("Height: "))
+
+
+
+
+i = 0
+
+while  i < val: 
+    string = ''
+    stringtwo= ''
+    i+= 1
+    for x in range(val -1):
+        string += ''
+      
+    for f in range(val - i):
+        stringtwo += " "
+      
+    for j in range(i):
+        string += "#"
+        stringtwo += '#'
+    print(stringtwo,string)
+
+         
+
+
+
+# python mariohard.py
